fe48847afbcb1514c5b27ef4c01b783db8c65f8c
Abdalla <abdallazwolle@hotmail.com>
2021-02-25 16:13:53 +0000

    Updates

---
+++ precache-manifest.c2fc355d43445e303deb7b49fecd26ea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e7214a7a8ec738bf3b0c4048ba65d2",
+    "url": "/Abdallas-portfolio/index.html"
+  },
+  {
+    "revision": "006050d936953c863f29",
+    "url": "/Abdallas-portfolio/static/css/main.a98450e8.chunk.css"
+  },
+  {
+    "revision": "a30c51251be45d1df1fb",
+    "url": "/Abdallas-portfolio/static/js/2.d269c1e2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Abdallas-portfolio/static/js/2.d269c1e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006050d936953c863f29",
+    "url": "/Abdallas-portfolio/static/js/main.bec94e98.chunk.js"
+  },
+  {
+    "revision": "f8a619b238ff27076537",
+    "url": "/Abdallas-portfolio/static/js/runtime-main.82cb9b33.js"
+  },
+  {
+    "revision": "1dfb656eae12f7532a7f04437cf3cf39",
+    "url": "/Abdallas-portfolio/static/media/backend.1dfb656e.png"
+  },
+  {
+    "revision": "3ae28f6a1fc018d09214acb075f5c649",
+    "url": "/Abdallas-portfolio/static/media/frontend.3ae28f6a.png"
+  },
+  {
+    "revision": "168c3b8ade1ff217d80e9714dceca8a1",
+    "url": "/Abdallas-portfolio/static/media/portfolio.168c3b8a.png"
+  },
+  {
+    "revision": "229f5279475d67e3cf902f7ab5cd818b",
+    "url": "/Abdallas-portfolio/static/media/video-1.229f5279.mp4"
+  }
+]);
